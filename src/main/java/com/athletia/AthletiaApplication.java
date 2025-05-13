456eae950e79f31e821dfe06d260d25e4d307df5
vicprandi <vicprandi@gmail.com>
2025-05-13 19:49:16 -0300

    Initial commit

---
+++ src/main/java/com/athletia/AthletiaApplication.java
@@ -0,0 +1,13 @@
+package com.athletia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthletiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthletiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athletia/TestAthletiaApplication.java
@@ -0,0 +1,11 @@
+package com.athletia;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAthletiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(AthletiaApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
