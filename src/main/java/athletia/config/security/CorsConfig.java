4a4a9eed7ff2d19f0c218cb7243d6ecc1ba29182
vicprandi <vicprandi@gmail.com>
2025-05-21 11:36:25 -0300

    health

--- src/main/java/athletia/config/security/CorsConfig.java
+++ src/main/java/athletia/config/security/CorsConfig.java
@@ -7,13 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("https://athletia2.onrender.com")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
@@ -21,3 +22,4 @@ public class CorsConfig {
         };
     }
 }
+
